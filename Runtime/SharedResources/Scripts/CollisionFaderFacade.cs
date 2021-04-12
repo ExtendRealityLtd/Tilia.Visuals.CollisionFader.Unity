8d736b4eb216c4ab2e714b27657beb2251ff98cb
extendreality <hello@extendreality.io>
2021-04-12 22:21:37 +0100

    feat(structure): add initial prefab, scripts and docs
    The initial structure for the repo has been added including
    the relevant prefab, scripts and documentation.

---
+++ Runtime/SharedResources/Scripts/CollisionFaderFacade.cs
@@ -0,0 +1,73 @@
+﻿namespace Tilia.Visuals.CollisionFader
+{
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Rule;
+
+    /// <summary>
+    /// The public interface for the CollisionFader prefab.
+    /// </summary>
+    public class CollisionFaderFacade : MonoBehaviour
+    {
+        #region Tracking Settings
+        /// <summary>
+        /// The source for the internal <see cref="Collider"/> to follow.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: Header("Tracking Settings"), DocumentedByXml]
+        public GameObject Source { get; set; }
+        /// <summary>
+        /// The rules to determine which scene cameras to apply the overlay to.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Cleared]
+        public RuleContainer CameraValidity { get; set; }
+        /// <summary>
+        /// The rules to determine which colliders in the scene will activate the fade.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Cleared]
+        public RuleContainer CollisionValidity { get; set; }
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public CollisionFaderConfigurator Configuration { get; protected set; }
+        #endregion
+
+        /// <summary>
+        /// Called after <see cref="Source"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(Source))]
+        protected virtual void OnAfterSourceChange()
+        {
+            Configuration.SetupFollower();
+        }
+
+        /// <summary>
+        /// Called after <see cref="CameraValidity"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(CameraValidity))]
+        protected virtual void OnAfterCameraValidityChange()
+        {
+            Configuration.SetupFadeOverlay();
+        }
+
+        /// <summary>
+        /// Called after <see cref="CollisionValidity"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(CollisionValidity))]
+        protected virtual void OnAfterCollisionValidityChange()
+        {
+            Configuration.SetupCollisionRule();
+        }
+    }
+}
