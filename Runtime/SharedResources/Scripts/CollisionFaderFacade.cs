a3074e44d87539b9921816b555bece3932b08b8f
Harvey Ball <me@harveyball.com>
2022-04-15 17:18:45 +0100

    feat(Malimbe): remove malimbe dependency
    BREAKING CHANGE: This removes the last remaining elements of
    Malimbe and whilst it does not cause any breaking changes within
    this package, it removes Malimbe as a dependency which other projects
    that rely on this package may piggy back off this Malimbe dependency
    so it will break any project like that.
    
    All of the previous functionality from Malimbe has been replicated in
    standard code without the need for it to be weaved by the Malimbe
    helper tags.

--- Runtime/SharedResources/Scripts/CollisionFaderFacade.cs
+++ Runtime/SharedResources/Scripts/CollisionFaderFacade.cs
@@ -1,11 +1,8 @@
 ﻿namespace Tilia.Visuals.CollisionFader
 {
-    using Malimbe.MemberChangeMethod;
-    using Malimbe.MemberClearanceMethod;
-    using Malimbe.PropertySerializationAttribute;
-    using Malimbe.XmlDocumentationAttribute;
     using UnityEngine;
     using Zinnia.Data.Attribute;
+    using Zinnia.Extension;
     using Zinnia.Rule;
 
     /// <summary>
@@ -14,39 +11,136 @@
     public class CollisionFaderFacade : MonoBehaviour
     {
         #region Tracking Settings
+        [Header("Tracking Settings")]
+        [Tooltip("The source for the internal Collider to follow.")]
+        [SerializeField]
+        private GameObject source;
         /// <summary>
         /// The source for the internal <see cref="Collider"/> to follow.
         /// </summary>
-        [Serialized, Cleared]
-        [field: Header("Tracking Settings"), DocumentedByXml]
-        public GameObject Source { get; set; }
+        public GameObject Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterSourceChange();
+                }
+            }
+        }
+        [Tooltip("The rules to determine which scene cameras to apply the overlay to.")]
+        [SerializeField]
+        private RuleContainer cameraValidity;
         /// <summary>
         /// The rules to determine which scene cameras to apply the overlay to.
         /// </summary>
-        [Serialized]
-        [field: DocumentedByXml, Cleared]
-        public RuleContainer CameraValidity { get; set; }
+        public RuleContainer CameraValidity
+        {
+            get
+            {
+                return cameraValidity;
+            }
+            set
+            {
+                cameraValidity = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterCameraValidityChange();
+                }
+            }
+        }
+        [Tooltip("The rules to determine which colliders in the scene will activate the fade.")]
+        [SerializeField]
+        private RuleContainer collisionValidity;
         /// <summary>
         /// The rules to determine which colliders in the scene will activate the fade.
         /// </summary>
-        [Serialized]
-        [field: DocumentedByXml, Cleared]
-        public RuleContainer CollisionValidity { get; set; }
+        public RuleContainer CollisionValidity
+        {
+            get
+            {
+                return collisionValidity;
+            }
+            set
+            {
+                collisionValidity = value;
+                if (this.IsMemberChangeAllowed())
+                {
+                    OnAfterCollisionValidityChange();
+                }
+            }
+        }
         #endregion
 
         #region Reference Settings
+        [Header("Reference Settings")]
+        [Tooltip("The linked Internal Setup.")]
+        [SerializeField]
+        [Restricted]
+        private CollisionFaderConfigurator configuration;
         /// <summary>
         /// The linked Internal Setup.
         /// </summary>
-        [Serialized]
-        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
-        public CollisionFaderConfigurator Configuration { get; protected set; }
+        public CollisionFaderConfigurator Configuration
+        {
+            get
+            {
+                return configuration;
+            }
+            protected set
+            {
+                configuration = value;
+            }
+        }
         #endregion
+
+        /// <summary>
+        /// Clears <see cref="Source"/>.
+        /// </summary>
+        public virtual void ClearSource()
+        {
+            if (!this.IsValidState())
+            {
+                return;
+            }
+
+            Source = default;
+        }
+
+        /// <summary>
+        /// Clears <see cref="CameraValidity"/>.
+        /// </summary>
+        public virtual void ClearCameraValidity()
+        {
+            if (!this.IsValidState())
+            {
+                return;
+            }
+
+            CameraValidity = default;
+        }
+
+        /// <summary>
+        /// Clears <see cref="CollisionValidity"/>.
+        /// </summary>
+        public virtual void ClearCollisionValidity()
+        {
+            if (!this.IsValidState())
+            {
+                return;
+            }
+
+            CollisionValidity = default;
+        }
 
         /// <summary>
         /// Called after <see cref="Source"/> has been changed.
         /// </summary>
-        [CalledAfterChangeOf(nameof(Source))]
         protected virtual void OnAfterSourceChange()
         {
             Configuration.SetupFollower();
@@ -55,7 +149,6 @@
         /// <summary>
         /// Called after <see cref="CameraValidity"/> has been changed.
         /// </summary>
-        [CalledAfterChangeOf(nameof(CameraValidity))]
         protected virtual void OnAfterCameraValidityChange()
         {
             Configuration.SetupFadeOverlay();
@@ -64,7 +157,6 @@
         /// <summary>
         /// Called after <see cref="CollisionValidity"/> has been changed.
         /// </summary>
-        [CalledAfterChangeOf(nameof(CollisionValidity))]
         protected virtual void OnAfterCollisionValidityChange()
         {
             Configuration.SetupCollisionRule();
